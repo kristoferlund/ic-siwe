e62e71d265391cffa4be89ad11a7d751815f0397
Kristofer <kristofer@fmckl.se>
2024-03-25 11:48:45 +0100

    Disable get functions if runtime features disallow mapping

--- packages/ic_siwe_provider/src/service/get_caller_address.rs
+++ packages/ic_siwe_provider/src/service/get_caller_address.rs
@@ -1,6 +1,8 @@
 use ic_cdk::query;
 use serde_bytes::ByteBuf;
 
+use crate::SETTINGS;
+
 use super::get_address::get_address;
 
 /// Retrieves the Ethereum address associated with the caller.
@@ -12,6 +14,13 @@ use super::get_address::get_address;
 /// * `Err(String)` - An error message if the principal cannot be converted or no address is found.
 #[query]
 fn get_caller_address() -> Result<String, String> {
+    SETTINGS.with_borrow(|s| {
+        if s.disable_principal_to_eth_mapping {
+            return Err("Principal to Ethereum address mapping is disabled".to_string());
+        }
+        Ok(())
+    })?;
+
     let principal = ic_cdk::caller();
     get_address(ByteBuf::from(principal.as_slice().to_vec()))
 }
