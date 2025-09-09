29baf7a8f3e4ac92fed17844da11b821522ad511
Kristofer Lund <kristofer@fmckl.se>
2025-09-09 15:19:06 +0200

    Replace manual ASN creation with ic_canister_sig_creaation

--- packages/ic_siwe/src/delegation.rs
+++ packages/ic_siwe/src/delegation.rs
@@ -1,5 +1,3 @@
-use std::{collections::HashMap, fmt};
-
 use super::hash::{self, Value};
 use crate::{
     eth::EthAddress,
@@ -8,13 +6,13 @@ use crate::{
     time::get_current_time,
     with_settings,
 };
-
-use ic_certified_map::{Hash, HashTree};
-use serde_bytes::ByteBuf;
-
 use candid::{CandidType, Principal};
+use ic_canister_sig_creation::CanisterSigPublicKey;
+use ic_certified_map::{Hash, HashTree};
 use serde::{Deserialize, Serialize};
-use simple_asn1::{from_der, oid, ASN1Block, ASN1EncodeErr};
+use serde_bytes::ByteBuf;
+use simple_asn1::from_der;
+use std::{collections::HashMap, fmt};
 
 #[derive(Debug)]
 pub enum DelegationError {
@@ -217,22 +215,8 @@ pub fn create_delegation_hash(delegation: &Delegation) -> Hash {
 ///
 /// # Returns
 /// Bytes of the DER-encoded public key.
-pub(crate) fn create_user_canister_pubkey(
-    canister_id: &Principal,
-    seed: Vec<u8>,
-) -> Result<Vec<u8>, ASN1EncodeErr> {
-    let canister_id: Vec<u8> = canister_id.as_slice().to_vec();
-
-    let mut key: Vec<u8> = vec![];
-    key.push(canister_id.len() as u8);
-    key.extend(canister_id);
-    key.extend(seed);
-
-    let algorithm = oid!(1, 3, 6, 1, 4, 1, 56387, 1, 2);
-    let algorithm = ASN1Block::Sequence(0, vec![ASN1Block::ObjectIdentifier(0, algorithm)]);
-    let subject_public_key = ASN1Block::BitString(0, key.len() * 8, key.to_vec());
-    let subject_public_key_info = ASN1Block::Sequence(0, vec![algorithm, subject_public_key]);
-    simple_asn1::to_der(&subject_public_key_info)
+pub(crate) fn create_user_canister_pubkey(canister_id: Principal, seed: Vec<u8>) -> Vec<u8> {
+    CanisterSigPublicKey::new(canister_id, seed).to_der()
 }
 
 /// Serializes data into CBOR format.
@@ -435,10 +419,8 @@ mod tests {
     fn test_create_user_canister_pubkey() {
         let address = init();
         let seed = generate_seed(&address);
-        let result =
-            create_user_canister_pubkey(&Principal::from_text("aaaaa-aa").unwrap(), seed.to_vec());
-        assert!(result.is_ok());
-        let pubkey = result.unwrap();
+        let pubkey =
+            create_user_canister_pubkey(Principal::from_text("aaaaa-aa").unwrap(), seed.to_vec());
         let result = from_der(&pubkey);
         assert!(
             result.is_ok(),
